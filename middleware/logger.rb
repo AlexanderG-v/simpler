e9a8048ce67e39378f54bb89e5020973c3069a3b
AlexanderG-v <gorelikov.alexander@gmail.com>
2021-12-20 19:36:44 +0300

    fixed logger

--- middleware/logger.rb
+++ middleware/logger.rb
@@ -9,7 +9,28 @@ class Applogger
   end
 
   def call(env)
-    @logger.info(env)
-    @app.call(env)
+    status, headers, body = @app.call(env)
+    request_info(env)
+    response_info(env, status, headers)
+
+    [status, headers, body]
+  end
+
+  def request_info(env)
+    @request = Rack::Request.new(env)
+    controller = env['simpler.controller'] if env['simpler.controller']
+    action = env['simpler.action'] if env['simpler.action']
+
+    log = "\nRequest: #{env['REQUEST_METHOD']} #{env['REQUEST_URI']}\n"
+    log += "Handler: #{controller}##{action}\n"
+    log += "Parameters: #{@request.params}"
+    @logger.info(log)
+  end
+
+  def response_info(env, status, headers)
+    log = "\nResponse: #{status} [#{headers['Content-Type']}] "
+    log += (env['simpler.view_file']).to_s if env['simpler.view_file']
+    log += "\n----------------------------------------------------"
+    @logger.info(log)
   end
 end
